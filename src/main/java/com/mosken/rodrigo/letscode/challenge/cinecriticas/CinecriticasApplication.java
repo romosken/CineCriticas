4381b3819d730595b726f9b6ca8217b49bcc73f2
romosken <rodrigo.mosken@outlook.com>
2022-06-22 20:36:32 -0300

    commit inicial

---
+++ src/main/java/com/mosken/rodrigo/letscode/challenge/cinecriticas/CinecriticasApplication.java
@@ -0,0 +1,13 @@
+package com.mosken.rodrigo.letscode.challenge.cinecriticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinecriticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinecriticasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mosken/rodrigo/letscode/challenge/cinecriticas/CinecriticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mosken.rodrigo.letscode.challenge.cinecriticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinecriticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
